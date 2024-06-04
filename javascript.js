7810e7fb1eb395b8ff0847fe20c3b5ea1c48300b
John Contreras <johndevproject@gmail.com>
2024-06-04 13:46:00 +0800

    Added click event

--- javascript.js
+++ javascript.js
@@ -34,3 +34,83 @@ function operate(numOne, opSign, numTwo) {
         return toDivide(numOne, numTwo);
     }
 }
+
+const firstDigit = document.querySelector(".first-num");
+const secondDiv = document.createElement("div");
+secondDiv.classList.add("second-num");
+const operatorDiv = document.createElement("div");
+operatorDiv.classList.add("operator");
+const resultDiv = document.querySelector(".result");
+resultDiv.appendChild(operatorDiv);
+resultDiv.appendChild(secondDiv);
+
+
+const divButtons = document.querySelectorAll(".buttons");
+divButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+
+        const opButton = document.querySelector(".operator");
+        const secondDigit = document.querySelector(".second-num");
+
+        if ((isNaN(parseFloat(button.innerHTML)) === false || button.innerHTML == ".") && opButton.innerHTML === "") {
+            if (firstDigit.innerHTML.includes(".") === true && button.innerHTML == ".") {
+                firstDigit.innerHTML += "";
+            } else {
+                firstDigit.innerHTML += button.innerHTML;
+            }
+            firstNum = firstDigit.innerHTML;
+        }
+
+        else if ((isNaN(parseFloat(button.innerHTML)) === true && button.innerHTML != "." && button.innerHTML != "=") && button.innerHTML != "C") {
+            if (operator === undefined) {
+                opButton.innerHTML = button.innerHTML;
+                operator = opButton.innerHTML;
+            } else if (operator != undefined) {
+                firstDigit.innerHTML = "";
+                secondDiv.innerHTML = "";
+                operatorDiv.innerHTML = "";
+                operate(firstNum, operator, secondNum);
+                opButton.innerHTML = button.innerHTML;
+                operator = opButton.innerHTML;
+                firstNum = total;
+                secondNum = 0;
+            }
+        }
+
+        else if ((isNaN(parseFloat(button.innerHTML)) === false || button.innerHTML == ".") && opButton.innerHTML != "") {
+            if (secondDiv.innerHTML.includes(".") === true && button.innerHTML == ".") {
+                firstDigit.innerHTML += "";
+            } else { secondDigit.innerHTML += button.innerHTML; }
+            secondNum = secondDiv.innerHTML;
+        }
+
+        else if (button.innerHTML == "C") {
+            firstDigit.innerHTML = "";
+            secondDiv.innerHTML = "";
+            operatorDiv.innerHTML = "";
+            operator = undefined;
+            firstNum = 0;
+            secondNum = 0;
+            total = undefined;
+        }
+
+
+        if (button.innerHTML == "=" && total === undefined) {
+            firstDigit.innerHTML = "";
+            secondDiv.innerHTML = "";
+            operatorDiv.innerHTML = "";
+            operate(firstNum, operator, secondNum);
+            operator = undefined;
+            firstNum = total;
+            secondNum = 0;
+        } else if (button.innerHTML == "=" && total != undefined) {
+            firstDigit.innerHTML = "";
+            secondDiv.innerHTML = "";
+            operatorDiv.innerHTML = "";
+            operate(total, operator, secondNum);
+            operator = undefined;
+            firstNum = total;
+            secondNum = 0;
+        }
+    });
+});
